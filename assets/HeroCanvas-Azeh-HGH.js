df265114eca1284dff7245c54e5cb86f3a5ed7f9
vanetiaho <vanetiaho@users.noreply.github.com>
2026-09-10 17:21:52 +0000

    deploy: 5f8670545709515b5d0c3ac57e9dba89ecdf80ab

--- assets/HeroCanvas-Azeh-HGH.js
+++ assets/HeroCanvas-Azeh-HGH.js
@@ -1,4 +1,4 @@
-import{r as Ve,g as Mx,j as Ye,u as Sx}from"./index-CsGnxHnZ.js";/**
+import{r as Ve,g as Mx,j as Ye,u as Sx}from"./index-DL47GQvG.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
