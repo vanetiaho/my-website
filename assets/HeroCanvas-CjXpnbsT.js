9ccbd1df6a6e00cd25563071e1536f2438dafb67
vanetiaho <vanetiaho@users.noreply.github.com>
2026-09-10 13:18:51 +0000

    deploy: 66c24c5666b65d62a3d8d1ae6f7f4957c42f4d21

--- assets/HeroCanvas-CjXpnbsT.js
+++ assets/HeroCanvas-CjXpnbsT.js
@@ -1,4 +1,4 @@
-import{r as Le,g as Mx,j as Et,u as Sx}from"./index-CK0hYVoz.js";/**
+import{r as Le,g as Mx,j as Et,u as Sx}from"./index-C38wenf5.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
